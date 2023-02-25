949a4867a6f78332f6f7a31b27d4ff13b3b5753e
Phillip-England <Phillip.mark.england@gmail.com>
2023-02-25 06:56:42 -0600

    added time to connectDb.go

--- net/ConnectDb.go
+++ net/ConnectDb.go
@@ -3,6 +3,7 @@ package net
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/phillip-england/go-http/model"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,7 +11,7 @@ import (
 )
 
 func ConnectDb() (db model.Db) {
-	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		cancel()
